2552c24fd57083f0f77b3cbd475adcf3880d8d8c
R E X Y <aryankc@R-E-X-Y-3.local>
2026-05-08 08:30:35 +0545

    db migration and entities added

--- SkillBridge.Api/Entities/User.cs
+++ SkillBridge.Api/Entities/User.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 namespace SkillBridge.Api.Entities
 {
     public class User
-    {}
+    {
 
         [Required, Key]
 
--- SkillBridge.Api/Program.cs
+++ SkillBridge.Api/Program.cs
@@ -1,16 +1,15 @@
+using Microsoft.AspNetCore.Connections;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddScoped<IJobRepository, JobRepository>();
+builder.Services.AddDbContext<SkillBridgeDbContext>(optios => optios.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
